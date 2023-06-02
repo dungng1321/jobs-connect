cfef7a7ae16370adf25433c72bf08683d7842d44
Dung Nguyen <dungnguyenhd1321@gmail.com>
2023-06-02 16:49:28 +0700

    Connect NestJS to MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://dungng1321:umagSLsVHzgdzIsD@cluster0.qoacmeo.mongodb.net/',
+    ),
+  ],
   controllers: [AppController, UserController],
   providers: [AppService],
 })
