b785441acf624d9b877eaa4110323a1a3013750b
Dung Nguyen <dungnguyenhd1321@gmail.com>
2023-06-09 15:55:19 +0700

    generate resources user

--- src/user/user.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Delete, Get } from '@nestjs/common';
-
-@Controller('user')
-export class UserController {
-  @Get()
-  findAll(): string {
-    return 'This action returns all users';
-  }
-}
