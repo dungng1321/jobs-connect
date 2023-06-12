398c3d5a0651ec047f6d065efec5442c9f43a077
Dung Nguyen <dungnguyenhd1321@gmail.com>
2023-06-12 20:16:58 +0700

    Validator DTO and optimize create user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,18 @@
+import { IsEmail, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
+
   age: number;
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,24 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
 import { hashPassword } from 'src/util/hashPassword';
+import {
+  MESSAGE_ERROR,
+  MESSAGE_SUCCESS,
+} from 'src/constants/constants.message';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
   // create new user width async await
-  async create(createUserDto: CreateUserDto): Promise<User> {
+  async create(createUserDto: CreateUserDto) {
     const { password, ...rest } = createUserDto;
+
+    const existingUser = await this.userModel.findOne({ email: rest.email });
+    if (existingUser) {
+      throw new BadRequestException(MESSAGE_ERROR.EMAIL_EXIST);
+    }
+
     const hashedPassword = await hashPassword(password);
-    const newUser = new this.userModel({
+    const newUser = await this.userModel.create({
       ...rest,
       password: hashedPassword,
     });
-    return newUser.save();
+
+    const data = newUser.toObject();
+
+    return {
+      statusCode: 200,
+      message: MESSAGE_SUCCESS.CREATE_NEW_USER_SUCCESS,
+      data,
+    };
   }
 
   findAll() {
