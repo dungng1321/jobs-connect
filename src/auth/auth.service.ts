5bda45657ae20ab6ec840947ab12625f320242f0
Dung Nguyen <dungnguyenhd1321@gmail.com>
2023-07-18 11:34:03 +0700

    setup auth by passport

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { User } from 'src/users/schemas/user.schema';
+import { comparePassword } from 'src/util/hashPassword';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async validateUser(username: string, password: string): Promise<any> {
+    const response = await this.usersService.findByUsername(username);
+    const user = response.data as User;
+
+    if (user && comparePassword(password, user.password)) {
+      return user;
+    }
+  }
+}
--- src/util/hashPassword.ts
+++ src/util/hashPassword.ts
@@ -1,7 +1,11 @@
-import { genSaltSync, hashSync } from 'bcryptjs';
+import { genSaltSync, hashSync, compareSync } from 'bcryptjs';
 
 export const hashPassword = (password: string) => {
   const salt = genSaltSync(10);
   const hashedPassword = hashSync(password, salt);
   return hashedPassword;
+};
+
+export const comparePassword = (password: string, hashedPassword: string) => {
+  return compareSync(password, hashedPassword);
 };
