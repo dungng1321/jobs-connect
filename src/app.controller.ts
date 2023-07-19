e419abd6123b38c7cdd685c5664fc15c33f1736a
Dung Nguyen <dungnguyenhd1321@gmail.com>
2023-07-19 12:38:13 +0700

    setup login with passport local (username, password)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Request } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
+import { UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
@@ -12,5 +14,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @UseGuards(LocalAuthGuard)
+  @Post('/login')
+  async login(@Request() req: any) {
+    return req.user;
   }
 }
