18558160451947284615b0dc9de197f8b9b22e32
Dung Nguyen <dungnguyenhd1321@gmail.com>
2023-07-26 15:23:42 +0700

    custom res message and Enable authentication globally

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,5 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  handleRequest(err: any, user: any, info: { message: string | undefined }) {
+    if (err || !user) {
+      throw err || new UnauthorizedException(info.message);
+    }
+    return user;
+  }
+}
