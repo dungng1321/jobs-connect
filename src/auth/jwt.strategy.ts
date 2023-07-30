81998ee08e062223abe5b8f568448a6180687510
Dung Nguyen <dungnguyenhd1321@gmail.com>
2023-07-31 03:07:26 +0700

    fix variable env

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -9,11 +9,18 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get<string>('JWT_SECRET'),
+      secretOrKey: configService.get<string>('JWT_AC_SECRET'),
     });
   }
 
   async validate(payload: any) {
-    return { userId: payload.sub, username: payload.username };
+    const expirationGMTPlus7 = new Date(
+      payload.exp * 1000 + 7 * 60 * 60 * 1000,
+    ).toISOString();
+    return {
+      userId: payload.sub,
+      username: payload.username,
+      expiration: expirationGMTPlus7,
+    };
   }
 }
