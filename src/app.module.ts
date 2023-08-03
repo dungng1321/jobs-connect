b45baf6aa014e70bf0f8718b7316c6497a1f931e
Dung Nguyen <dungnguyenhd1321@gmail.com>
2023-08-03 08:32:53 +0700

    setup soft delete and config enable CORS

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { softDeletePlugin } from 'soft-delete-plugin-mongoose';
 
 @Module({
   imports: [
     MongooseModule.forRootAsync({
       useFactory: (configService: ConfigService) => ({
         uri: configService.get<string>('MONGO_URI'),
+        connectionFactory: (connection: any) => {
+          connection.plugin(softDeletePlugin);
+          return connection;
+        },
       }),
       inject: [ConfigService],
     }),
