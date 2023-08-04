1df716053db913ee6c9d784458ec545af2d453aa
Dung Nguyen <dungnguyenhd1321@gmail.com>
2023-08-04 08:26:58 +0700

    rename database and  create new company api

--- src/companies/companies.module.ts
+++ src/companies/companies.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CompanySchema } from './schemas/company.schema';
+import { Company, CompanySchema } from './schemas/company.schema';
 import { CompaniesService } from './companies.service';
 import { CompaniesController } from './companies.controller';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: 'Company', schema: CompanySchema }]),
+    MongooseModule.forFeature([{ name: Company.name, schema: CompanySchema }]),
   ],
   controllers: [CompaniesController],
   providers: [CompaniesService],
